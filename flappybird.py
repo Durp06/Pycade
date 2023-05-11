6b5fb72a67ad1a8049429bb3570b2fcc260eeec6
Durp06 <slyles2006@gmail.com>
2023-05-11 09:21:24 -0400

    added sprites

--- flappybird.py
+++ flappybird.py
@@ -1,46 +1,25 @@
 import pygame
 import sys
 import os
+import random
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
-clock = pygame.time.Clock()
+
+window_width = 600
+window_height = 500
 running = True
 dt = 0
+
+screen = pygame.display.set_mode((window_width, window_height))
+clock = pygame.time.Clock()
+fps = 60
 directory = os.getcwd()
+
+#sprites
 bg = pygame.image.load(f"{directory}\\flap-sprites\\background.jpg")
+pipe = pygame.image.load(f"{directory}\\flap-sprites\\pipe.png")
+bird = pygame.image.load(f"{directory}\\flap-sprites\\bird.png")
+base = pygame.image.load(f"{directory}\\flap-sprites\\base.png")
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "blue", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
+pygame.display.set_caption("Flappy Bird")
